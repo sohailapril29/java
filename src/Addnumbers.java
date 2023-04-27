6614253ce4f97f4737fca41384fed1b23314c3ab
Mohammed Sohail <mohammedsohailapril292003@gmail.com>
2023-04-27 12:16:18 +0530

    updated

--- src/Addnumbers.java
+++ src/Addnumbers.java
@@ -1,5 +1,5 @@
 public class Addnumbers {
     public static void main(String[] args) {
-        System.out.println("hello add numbers");
+        System.out.println("Hello Add numbers latest");
     }
 }
