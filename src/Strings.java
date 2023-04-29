a56d3cf1e8c390a6b660d21da4735825917a9c10
Mohammed Sohail <mohammedsohailapril292003@gmail.com>
2023-04-29 13:42:09 +0530

    video 14 start

--- src/Strings.java
+++ src/Strings.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class Strings {
     public static void main(String[] args) {
-        System.out.println("Hello Strings");
+        int a = 6;
+        float b = 5.6454f;
+        char c = (char) 'Q';
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your name");
+        String name = sc.nextLine();
+        System.out.printf("The value of integer is %d and value of " +
+                "float is %f The value of char is %c and The value of String is %s"
+                , a,b,c,name);
     }
 }
