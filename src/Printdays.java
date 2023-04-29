50443ac71a5929f420f0776b5016f272b64372b8
Mohammed Sohail <mohammedsohailapril292003@gmail.com>
2023-04-29 12:33:49 +0530

    pdf added from 1 to 20 .

---
+++ src/Printdays.java
@@ -0,0 +1,9 @@
+public class Printdays {
+    public static void main(String[] args) {
+        String days = "Day";
+        for (int i = 0; i < 60; i++) {
+            System.out.println(days);
+        }
+
+    }
+}
