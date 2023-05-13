389be90739650ddfc72c8051c0980e36ffd73119
Mohammed Sohail <mohammedsohailapril292003@gmail.com>
2023-05-13 18:47:55 +0530

    start 23

---
+++ src/Sohailwhileloops.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+public class Sohailwhileloops {
+    //    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter The Natural Number");
+//        int numbers = sc.nextInt();
+//        while (numbers<=200) {
+//            System.out.println("Number " + numbers);
+//            numbers++;
+//        }
+//    }
+
+
+
+//    public static void main(String[] args) {
+//    // Write a program to print numbers from 1 to 10.
+//    int a = 1;
+//    while(a<=10){
+//        System.out.println(a);
+//        a++;
+//    }
+//    }
+
+//using do while print n natural number
+
+//    public static void main(String[] args) {
+//        int a = 1;
+//        do {
+//            System.out.println("Your Natural Number " + a);
+//            a++;
+//        } while (a<=100);
+//    }
+
+//print odd natural numbers using for loop
+
+//    public static void main(String[] args) {
+//        int num = 10;
+//        for (int i = 1; i<=num; i++){
+//            if (i%2!=0){
+//                System.out.println("Your Frist Natural Number " + i);
+//            }
+//        }
+//    }
+
+ //using while print 10 number from 1 to 10
+
+//    public static void main(String[] args) {
+//        int i = 1;
+//        while (i<=10) {
+//            System.out.println("Your While Number " + i);
+//            i++;
+//        }
+//    }
+
+// print number from 1 to 10 using do while loop
+
+//    public static void main(String[] args) {
+//        int i = 1;
+//        do {
+//            System.out.println("Your do-while number is " + i);
+//            i++;
+//        } while (i<=10);
+//    }
+
+// print 1 to 10 using for loop
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Print The Number");
+        int a = sc.nextInt();
+        int b =0;
+//        while(a<=10) {
+//            System.out.println(a);
+//            a++;
+//        }
+
+//        for (int i=0; i<=a; i++) {
+//            System.out.println(i);
+//        }
+
+//        do {
+//            System.out.println(b);
+//            b++;
+//        } while (b<=a);
+
+        System.out.println("sohail");
+    }
+}
